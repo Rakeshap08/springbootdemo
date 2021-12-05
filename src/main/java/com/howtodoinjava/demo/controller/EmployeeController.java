5c4b533458c8155b30e3caf4904780332199ba8a
Rakesha P <rakeshap41@gmail.com>
2021-12-05 11:23:00 +0530

    Added Logger

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package com.howtodoinjava.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +13,20 @@ import com.howtodoinjava.demo.model.Employee;
 
 @RestController
 public class EmployeeController {
+
+	Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 	
 	@RequestMapping("/employee/list")
     public List<Employee> getEmployees() 
     {
-		System.out.println("Hello");
-		System.out.println();
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+
+		logger.info("Enter Employee Controller......");
 		List<Employee> employeesList = new ArrayList<Employee>();
-		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com", "9008101522"));
-		System.out.println();
-		System.out.println("World");
+		employeesList.add(new Employee(1,"lokesh","gupta","test@gmail.com", "9008101522"));
+		logger.info("Exit Employee Controller......");
+		//logger.info("ends in {} stopWatch.stop());
 		return employeesList;
     }
 
